5a88f3325483703b27c019e629cb00a2be480926
nithin-opine <nithin.opine@gmail.com>
2022-04-04 20:18:43 +0530

    added files

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -34,7 +34,7 @@ import { loginUser, socialLogin } from "../../store/actions"
 
 // import images
 import profile from "assets/images/profile-img.png"
-import logo from "assets/images/logo.svg"
+import logo from "assets/ondenassets/logo.png"
 
 //Import config
 import { facebook, google } from "../../config"
@@ -105,216 +105,100 @@ const Login = props => {
   return (
     <React.Fragment>
       <MetaTags>
-        <title>Login | Skote - React Admin & Dashboard Template</title>
+        <title>Login | Onden Admin</title>
       </MetaTags>
-      <div className="home-btn d-none d-sm-block">
-        <Link to="/" className="text-dark">
-          <i className="fas fa-home h2" />
-        </Link>
-      </div>
-      <div className="account-pages my-5 pt-sm-5">
-        <Container>
-          <Row className="justify-content-center">
-            <Col md={8} lg={6} xl={5}>
-              <Card className="overflow-hidden">
-                <div className="bg-primary bg-soft">
-                  <Row>
-                    <Col xs={7}>
-                      <div className="text-primary p-4">
-                        <h5 className="text-primary">Welcome Back !</h5>
-                        <p>Sign in to continue to Skote.</p>
-                      </div>
-                    </Col>
-                    <Col className="col-5 align-self-end">
-                      <img src={profile} alt="" className="img-fluid" />
-                    </Col>
-                  </Row>
+      <Container fluid>
+        <Row>
+          <Col md={12} className="loginBg">
+            <Row>
+              <Col md={4} className="loginformcol">
+                <div className="logocontent">
+                  <img src={logo} alt="" />
                 </div>
-                <CardBody className="pt-0">
-                  <div>
-                    <Link to="/" className="auth-logo-light">
-                      <div className="avatar-md profile-user-wid mb-4">
-                        <span className="avatar-title rounded-circle bg-light">
-                          <img
-                            src={logo}
-                            alt=""
-                            className="rounded-circle"
-                            height="34"
-                          />
-                        </span>
-                      </div>
-                    </Link>
+                <Form
+                  className="form-horizontal"
+                  onSubmit={e => {
+                    e.preventDefault()
+                    validation.handleSubmit()
+                    return false
+                  }}
+                >
+                  {error ? <Alert color="danger">{error}</Alert> : null}
+
+                  <div className="mb-3">
+                    <Label className="form-label">username</Label>
+                    <Input
+                      name="username"
+                      className="form-control"
+                      placeholder="Enter username"
+                      type="text"
+                      onChange={validation.handleChange}
+                      onBlur={validation.handleBlur}
+                      value={validation.values.username || ""}
+                      invalid={
+                        validation.touched.username &&
+                        validation.errors.username
+                          ? true
+                          : false
+                      }
+                    />
+                    {validation.touched.username &&
+                    validation.errors.username ? (
+                      <FormFeedback type="invalid">
+                        {validation.errors.username}
+                      </FormFeedback>
+                    ) : null}
                   </div>
-                  <div className="p-2">
-                    <Form
-                      className="form-horizontal"
-                      onSubmit={e => {
-                        e.preventDefault()
-                        validation.handleSubmit()
-                        return false
-                      }}
+
+                  <div className="mb-3">
+                    <Label className="form-label">Password</Label>
+                    <Input
+                      name="password"
+                      value={validation.values.password || ""}
+                      type="password"
+                      placeholder="Enter Password"
+                      onChange={validation.handleChange}
+                      onBlur={validation.handleBlur}
+                      invalid={
+                        validation.touched.password &&
+                        validation.errors.password
+                          ? true
+                          : false
+                      }
+                    />
+                    {validation.touched.password &&
+                    validation.errors.password ? (
+                      <FormFeedback type="invalid">
+                        {validation.errors.password}
+                      </FormFeedback>
+                    ) : null}
+                  </div>
+
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="customControlInline"
+                    />
+                    <label
+                      className="form-check-label"
+                      htmlFor="customControlInline"
                     >
-                      {error ? <Alert color="danger">{error}</Alert> : null}
-
-                      <div className="mb-3">
-                        <Label className="form-label">username</Label>
-                        <Input
-                          name="username"
-                          className="form-control"
-                          placeholder="Enter username"
-                          type="text"
-                          onChange={validation.handleChange}
-                          onBlur={validation.handleBlur}
-                          value={validation.values.username || ""}
-                          invalid={
-                            validation.touched.username &&
-                            validation.errors.username
-                              ? true
-                              : false
-                          }
-                        />
-                        {validation.touched.username &&
-                        validation.errors.username ? (
-                          <FormFeedback type="invalid">
-                            {validation.errors.username}
-                          </FormFeedback>
-                        ) : null}
-                      </div>
-
-                      <div className="mb-3">
-                        <Label className="form-label">Password</Label>
-                        <Input
-                          name="password"
-                          value={validation.values.password || ""}
-                          type="password"
-                          placeholder="Enter Password"
-                          onChange={validation.handleChange}
-                          onBlur={validation.handleBlur}
-                          invalid={
-                            validation.touched.password &&
-                            validation.errors.password
-                              ? true
-                              : false
-                          }
-                        />
-                        {validation.touched.password &&
-                        validation.errors.password ? (
-                          <FormFeedback type="invalid">
-                            {validation.errors.password}
-                          </FormFeedback>
-                        ) : null}
-                      </div>
-
-                      <div className="form-check">
-                        <input
-                          type="checkbox"
-                          className="form-check-input"
-                          id="customControlInline"
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="customControlInline"
-                        >
-                          Remember me
-                        </label>
-                      </div>
-
-                      <div className="mt-3 d-grid">
-                        <button
-                          className="btn btn-primary btn-block"
-                          type="submit"
-                        >
-                          Log In
-                        </button>
-                      </div>
-
-                      <div className="mt-4 text-center">
-                        <h5 className="font-size-14 mb-3">Sign in with</h5>
-
-                        <ul className="list-inline">
-                          <li className="list-inline-item">
-                            <FacebookLogin
-                              appId={facebook.APP_ID}
-                              autoLoad={false}
-                              callback={facebookResponse}
-                              render={renderProps => (
-                                <Link
-                                  to="#"
-                                  className="social-list-item bg-primary text-white border-primary"
-                                  onClick={renderProps.onClick}
-                                >
-                                  <i className="mdi mdi-facebook" />
-                                </Link>
-                              )}
-                            />
-                          </li>
-                          {/*<li className="list-inline-item">*/}
-                          {/*  <TwitterLogin*/}
-                          {/*    loginUrl={*/}
-                          {/*      "http://localhost:4000/api/v1/auth/twitter"*/}
-                          {/*    }*/}
-                          {/*    onSuccess={this.twitterResponse}*/}
-                          {/*    onFailure={this.onFailure}*/}
-                          {/*    requestTokenUrl={*/}
-                          {/*      "http://localhost:4000/api/v1/auth/twitter/revers"*/}
-                          {/*    }*/}
-                          {/*    showIcon={false}*/}
-                          {/*    tag={"div"}*/}
-                          {/*  >*/}
-                          {/*    <a*/}
-                          {/*      href=""*/}
-                          {/*      className="social-list-item bg-info text-white border-info"*/}
-                          {/*    >*/}
-                          {/*      <i className="mdi mdi-twitter"/>*/}
-                          {/*    </a>*/}
-                          {/*  </TwitterLogin>*/}
-                          {/*</li>*/}
-                          <li className="list-inline-item">
-                            <GoogleLogin
-                              clientId={google.CLIENT_ID}
-                              render={renderProps => (
-                                <Link
-                                  to="#"
-                                  className="social-list-item bg-danger text-white border-danger"
-                                  onClick={renderProps.onClick}
-                                >
-                                  <i className="mdi mdi-google" />
-                                </Link>
-                              )}
-                              onSuccess={googleResponse}
-                              onFailure={() => {}}
-                            />
-                          </li>
-                        </ul>
-                      </div>
-
-                      <div className="mt-4 text-center">
-                        <Link to="/forgot-password" className="text-muted">
-                          <i className="mdi mdi-lock me-1" />
-                          Forgot your password?
-                        </Link>
-                      </div>
-                    </Form>
+                      Remember me
+                    </label>
                   </div>
-                </CardBody>
-              </Card>
-              <div className="mt-5 text-center">
-                <p>
-                  Don&#39;t have an account ?{" "}
-                  <Link to="/register" className="fw-medium text-primary">
-                    {" "}
-                    Signup now{" "}
-                  </Link>{" "}
-                </p>
-                <p>
-                  © {new Date().getFullYear()} Skote. Crafted with{" "}
-                  <i className="mdi mdi-heart text-danger" /> by Themesbrand
-                </p>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+
+                  <div className="mt-3 d-grid">
+                    <button className="btn btn-primary btn-block" type="submit">
+                      Log In
+                    </button>
+                  </div>
+                </Form>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </React.Fragment>
   )
 }
